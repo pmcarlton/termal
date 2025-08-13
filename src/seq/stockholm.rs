597c45834661d5c2e3f4dda8c487f6c26eb4f01c
Thomas Junier <thomas.junier@sib.swiss>
2025-08-13 13:36:09 +0200

    + new tests or Stockholm fmt: all ok

--- src/seq/stockholm.rs
+++ src/seq/stockholm.rs
@@ -3,7 +3,7 @@
 
 use std::fs::File;
 use std::path::Path;
-use std::io::{BufRead, BufReader};
+use std::io::{BufRead, BufReader, Error, ErrorKind};
 
 use crate::seq::record::SeqRecord;
 use crate::seq::file::SeqFile;
@@ -11,26 +11,29 @@ use crate::seq::file::SeqFile;
 pub fn read_stockholm_file<P: AsRef<Path>>(path: P) -> Result<SeqFile, std::io::Error> {
     let file = File::open(path)?;
     let mut result: SeqFile = Vec::new();
-    let mut current_record = SeqRecord { header: String::new(), sequence: String::new() };
-    let mut first_header = true;
 
     for line in BufReader::new(file).lines() {
         let l: String = line.unwrap();
-        if l.starts_with(">") { 
-            if first_header {
-                first_header = false;
-            } else {
-                // push existing record
-                result.push(current_record);
+        let first_char = l.chars().next().unwrap();
+        match first_char {
+            '/' => { break; } // Assuming '/' is the beginning of '//', which conceivably might not be
+                          // true
+            '#' => {} // Annotation -> ignore.
+            _ => {
+                let mut fields = l.split_whitespace();
+
+                match (fields.next(), fields.next(), fields.next()) {
+                    (Some(seqname), Some(aln_seq), None) => {
+                        let record = SeqRecord { header: String::from(seqname), sequence: String::from(aln_seq) };
+                        result.push(record);
+                    }
+                    // TODO: use a specific kind of Error for this, not a std::io::Error.
+                    _ => return Err(Error::new(ErrorKind::InvalidData, "Expected exactly two fields"))
+                }
             }
-            current_record = SeqRecord { header: String::new(), sequence: String::new() };
-            current_record.header.push_str(&l[1..]);
-        } else {
-            // append line to current record'd sequence
-            current_record.sequence.push_str(&l);
         }
     }
-    result.push(current_record);
+
     Ok(result)
 }
 
@@ -39,11 +42,26 @@ mod tests {
     use super::*;
 
     #[test]
-    fn test_read_stockholm_file_1() {
+    fn test_read_stockholm_file_len() {
         let path = "data/PF00571.sto";
         let fasta: SeqFile = read_stockholm_file(path).expect("Test file not found");
-        assert_eq!(fasta[0].header, "seq1");
-        assert_eq!(fasta[0].sequence, "GAATTC");
+        assert_eq!(fasta.len(), 5);
+    }
+
+    #[test]
+    fn test_read_stockholm_file_1st_record() {
+        let path = "data/PF00571.sto";
+        let fasta: SeqFile = read_stockholm_file(path).expect("Test file not found");
+        assert_eq!(fasta[0].header, "O83071/192-246");
+        assert_eq!(fasta[0].sequence, "MTCRAQLIAVPRASSLAE..AIACAQKM....RVSRVPVYERS");
+    }
+
+    #[test]
+    fn test_read_stockholm_file_last_record() {
+        let path = "data/PF00571.sto";
+        let fasta: SeqFile = read_stockholm_file(path).expect("Test file not found");
+        assert_eq!(fasta[4].header, "O31699/88-139");
+        assert_eq!(fasta[4].sequence, "EVMLTDIPRLHINDPIMK..GFGMVINN......GFVCVENDE");
     }
 
     // TODO: more tests
